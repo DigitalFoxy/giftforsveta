3a4223c692b00852b838b57b744f5406cdc6819c
DigitalFoxy <90634529+DigitalFoxy@users.noreply.github.com>
2022-05-06 21:59:26 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+setInterval(function makeCat() {
+	document.getElementById("meow").src = "img/meow-content.png";
+	setTimeout(function makeCatDefault() {document.getElementById("meow").src = "img/meow_neutral.png"}, 1000)
+},2500);
+
+function makefon1() {
+document.getElementById("title").style.background = "linear-gradient(85deg,khaki,cornsilk)"
+document.getElementById("footer").style.background = "linear-gradient(-85deg,khaki,cornsilk)"
+}
+function makefon2() {
+document.getElementById("title").style.background = "linear-gradient(85deg,greenyellow,cornsilk)"
+document.getElementById("footer").style.background = "linear-gradient(-85deg,greenyellow,cornsilk)"
+}
+function makefon3() {
+document.getElementById("title").style.background = "linear-gradient(85deg,rosybrown,cornsilk)"
+document.getElementById("footer").style.background = "linear-gradient(-85deg,rosybrown,cornsilk)"
+}
+
+function makeCatMad() {
+	document.getElementById("meow").src = "img/meow-mad.png";
+}
+function makeCatThinking() {
+	document.getElementById("meow").src = "img/meow-thinking.png";
+}
+function makeCatLol() {
+	document.getElementById("meow").src = "img/meow-lol.png";
+}
+function makeCatCrying() {
+	document.getElementById("meow").src = "img/meow-crying.png";
+}
